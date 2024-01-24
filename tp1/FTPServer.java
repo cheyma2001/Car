edb64dfb25202b7ca9d757872d3f44c9a3f92ff1
badr <bedro.salmi@gmail.com>
2024-01-24 20:23:10 +0100

    GET cmd

--- tp1/FTPServer.java
+++ tp1/FTPServer.java
@@ -1,12 +1,12 @@
 package tp1;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.*;
 import java.util.HashMap;
 import java.util.Scanner;
-
-
 
 public class FTPServer{
 	 private static final HashMap<String,String> userCredentials = new HashMap<>();
@@ -42,27 +42,38 @@ public class FTPServer{
 		
 			 while (true) {
 				 String command = scanner.next().toUpperCase();
-				 
+				 System.out.println(command);
 				 switch (command) {
 				 case "USER":
+				 System.out.println(command);
 					 handleUserCommand(scanner, outputStream);
 				 break;
 				 case "PASS":
+				 System.out.println(command);
 					 handlePassCommand(scanner, outputStream);
 				 break;
+                 case "RETR":
+				 System.out.println(command);
+                 handleRetrCommand(scanner, outputStream,clientSocket);
+                 break;
 				 case "QUIT":
+				 System.out.println(command);
 				 handleQuitCommand(outputStream, clientSocket);
 				 return;
-				 default:
-					 sendResponse(outputStream, "500 Syntax error, command not recognized.\r\n");
+				 
+				default:
+					sendResponse(outputStream, "500 Syntax error, command not recognized.\r\n");
 				 }
 			 }
+			
 		 }
+		 
 	 }
 
 	 private static void handleUserCommand(Scanner scanner, OutputStream outputStream) throws IOException {
 	 String username = scanner.next();
 	 if (testConnectionUserName(username)){
+    System.out.println("USER "+username);
 	 sendResponse(outputStream, "331 User " + username + " OK. Password required.\r\n");
 	 }
 	 else{
@@ -75,6 +86,7 @@ public class FTPServer{
 	 String password = scanner.next();
 	 
 	 if (testConnectionUserPass(password)) {
+    System.out.println("PASS "+password);
 	 sendResponse(outputStream, "230 User logged in.\r\n");
 	 } else {
 	 sendResponse(outputStream, "530 Login incorrect.\r\n");
@@ -99,4 +111,37 @@ public class FTPServer{
 	 private static void sendResponse(OutputStream outputStream, String response) throws IOException {
 	 outputStream.write(response.getBytes());
 	 }
-	}
+     private static void handleRetrCommand(Scanner scanner, OutputStream outputStream,Socket clientSocket) throws IOException {
+        String fileName = scanner.next().trim();
+        File file = new File(fileName);
+        System.out.println(file.exists());
+        if (!file.exists()) {
+            sendResponse(outputStream, "550 File not found\r\n");
+            return;
+        }
+		try {
+            sendResponse(outputStream, "150 Opening data connection for RETR\r\n");
+
+            // Accept the data connection for file transfer
+            try (Socket dataSocket = new Socket(clientSocket.getInetAddress(), 2020);
+                 FileInputStream fileInputStream = new FileInputStream(file);
+                 OutputStream dataOutputStream = dataSocket.getOutputStream()) {
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+
+                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                    dataOutputStream.write(buffer, 0, bytesRead);
+                }
+
+                sendResponse(outputStream, "226 Transfer complete\r\n");
+            } catch (IOException e) {
+                sendResponse(outputStream, "552 Requested file action aborted\r\n");
+            }
+        } catch (IOException e) {
+            sendResponse(outputStream, "552 Requested file action aborted\r\n");
+        }
+    }
+     
+     
+}
